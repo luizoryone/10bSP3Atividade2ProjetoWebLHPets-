c5cb0eaef51eca345501a2d0509034d8819e8730
luiz oryone <luizoryone@gmail.com>
2024-09-08 16:01:04 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Projeto_Web_Lh_Pets_versão_1;
+
+namespace code;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Projeto Web - LH Pests versão 1");
+        app.UseStaticFiles();
+        app.MapGet("/index",(HttpContext contexto) => {
+            contexto.Response.Redirect("index.html", false);
+        
+        app.UseStaticFiles();
+            app.MapGet("/index", (HttpContext contexto) =>
+            {
+                contexto.Response.Redirect("index.html", false);
+            });    
+        });
+
+        Banco dba = new Banco();
+
+        app.MapGet("/listaClientes", (HttpContext contexto) =>{
+            contexto.Response.WriteAsync(dba.GetListaString());
+            
+
+        });
+
+        app.Run();
+    }
+}
